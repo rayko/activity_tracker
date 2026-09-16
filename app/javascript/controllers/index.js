c22b89bbb481888a67bb9ec6ff60c1c59080b0af
rayko <131372+rayko@users.noreply.github.com>
2026-09-16 16:51:10 -0300

    Adding some npm packages

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,10 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+
+// Not sure if this is actually needed, the line above does that already?
+// import { registerControllers } from 'stimulus-vite-helpers'
+// // Automatically import and register all controllers inside this directory
+// const controllers = import.meta.glob('./**/*_controller.js', { eager: true })
+// registerControllers(application, controllers)

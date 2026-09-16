f54bfdbbcdb04c2aa1cae1361fe8a3b8d55c33ca
rayko <131372+rayko@users.noreply.github.com>
2026-09-16 16:51:10 -0300

    Fixing some redirects and adding confirm for deleting activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -53,7 +53,7 @@ class ActivitiesController < ApplicationController
     @activity.destroy!
 
     respond_to do |format|
-      format.html { redirect_to activities_path, notice: "Activity was successfully destroyed.", status: :see_other }
+      format.html { redirect_to root_path, notice: "Activity was successfully destroyed.", status: :see_other }
       format.json { head :no_content }
     end
   end

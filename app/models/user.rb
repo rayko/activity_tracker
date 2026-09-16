3e44629362529ac8c46940606cd7f485a4d1ab45
rayko <131372+rayko@users.noreply.github.com>
2026-09-16 16:51:10 -0300

    Adding signout button

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+  devise :database_authenticatable, :rememberable, :validatable, :trackable
 end

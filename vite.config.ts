7de0af72fcbfb79a9ed4c013f1872fd04f3d219c
rayko <131372+rayko@users.noreply.github.com>
2026-09-16 16:51:10 -0300

    Using default assets folder in vite and tested sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@assets': resolve(__dirname, 'app/assets'),
+    },
+  },
   plugins: [
     RubyPlugin(),
   ],

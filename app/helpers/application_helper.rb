0c4fae9c432655ec72de5c2f83b2142633088c32
rayko <131372+rayko@users.noreply.github.com>
2026-09-16 16:51:10 -0300

    Adding some styling to activity views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,10 @@ module ApplicationHelper
     "block text-center mx-2 rounded-md bg-linear-to-t from-sky-700 to-blue-400 text-gray-200 font-bold outline-indigo-300 px-3 py-1 hover:outline-2 hover:outline-sky-500 text-shadow-md/30"
   end
 
+  def regular_red_btn_class
+    "block text-center mx-2 rounded-md bg-linear-to-t from-red-700 to-red-400 text-gray-200 font-bold outline-red-300 px-3 py-1 hover:outline-2 hover:outline-red-500 text-shadow-md/30"
+  end
+  
   def login_link_btn(text, target)
     content_tag(:div, class: "my-4 w-full text-center") do
       link_to text, target, class: blue_btn_class

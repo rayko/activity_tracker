b19048582eb8a1e8b3b0366dcfec6ca5b6afdd4e
rayko <131372+rayko@users.noreply.github.com>
2026-09-16 16:51:11 -0300

    Testing activity show view

--- spec/views/activities/show.html.erb_spec.rb
+++ spec/views/activities/show.html.erb_spec.rb
@@ -1,11 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "activities/show", type: :view do
+  let(:activity) { create :activity }
+
   before(:each) do
-    assign(:activity, create(:activity))
+    assign(:activity, activity)
   end
 
-  it "renders attributes in <p>" do
-    render
+  it "displays activity name" do
+    expect(render).to match /#{activity.name}/
+  end
+
+  it 'displays activity description' do
+    expect(render).to match /#{activity.description}/
+  end
+
+  it 'displays Edit button' do
+    expect(render).to have_link("Edit", href: edit_activity_path(activity))
+  end
+
+  it 'displays Destroy button' do
+    expect(render).to have_button("Destroy")
   end
 end
